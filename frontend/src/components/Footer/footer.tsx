862737cd926fca6661e53a5f47169d6151b91401
Nabil Sahsada Suratno <nabilsahsadacode@gmail.com>
2025-02-02 22:53:34 +0700

    fix : footer name

--- frontend/src/components/Footer/footer.tsx
+++ frontend/src/components/Footer/footer.tsx
@@ -1,9 +1,8 @@
 const Footer = () => {
-    const websiteName = process.env.REACT_APP_WEBSITE_NAME;
     return (
         <footer className="fixed bottom-0 left-0 w-full footer footer-center bg-pink-500 p-4 text-white">
             <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved by {websiteName}</p>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by Naffie's Restaurant</p>
             </aside>
         </footer>
     );
